69867f4bcb63a5c4e09135fa311cb9efd2dae8a3
zhu-qin <qqinzhu@gmail.com>
2016-12-07 10:11:48 -0500

    NXP-21037: deleted unnecessary files

--- react-sample-mp/ftest/webdriverio/tests/specs/test-sample-ui.js
+++ react-sample-mp/ftest/webdriverio/tests/specs/test-sample-ui.js
@@ -2,7 +2,7 @@ var correctLogIn = require('./test-login.js');
 var assert = require('chai').assert;
 
 describe('should navigate to Sample UI', function () {
-    it.only ('should be on the Sample UI page', function () {
+    it('should be on the Sample UI page', function () {
         correctLogIn(browser)
             .url('/nuxeo/sampleUI')
             .click('.login-button')
